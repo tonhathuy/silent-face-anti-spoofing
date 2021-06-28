6b53fab59777a5a5b1c05e7a3ab02cbd3ae7cecf
tonhathuy <tonhathuy97@gmail.com>
2021-06-28 21:56:49 +0700

    add request face croped

--- app_v2.py
+++ app_v2.py
@@ -58,9 +58,6 @@ def test(image_name, image):
     
     image_cropper = CropImage()
     result = check_image(image)
-    # if result is False:
-    #     print("check")
-    #     return
     image_bbox = model_test.get_bbox(image)
     prediction = np.zeros((1, 3))
     test_speed = 0
@@ -85,29 +82,34 @@ def test(image_name, image):
     # draw result of prediction
     label = np.argmax(prediction)
     value = prediction[0][label]/2
-    # if label == 1:
-    #     print("Image '{}' is Real Face. Score: {:.2f}.".format(image_name, value))
-    #     result_text = "RealFace Score: {:.2f}".format(value)
-    #     color = (255, 0, 0)
-    # else:
-    #     print("Image '{}' is Fake Face. Score: {:.2f}.".format(image_name, value))
-    #     result_text = "FakeFace Score: {:.2f}".format(value)
-    #     color = (0, 0, 255)
-    # print("Prediction cost {:.2f} s".format(test_speed))
-    # cv2.rectangle(
-    #     image,
-    #     (image_bbox[0], image_bbox[1]),
-    #     (image_bbox[0] + image_bbox[2], image_bbox[1] + image_bbox[3]),
-    #     color, 2)
-    # cv2.putText(
-    #     image,
-    #     result_text,
-    #     (image_bbox[0], image_bbox[1] - 5),
-    #     cv2.FONT_HERSHEY_COMPLEX, 0.5*image.shape[0]/1024, color)
+    return label, value
 
-    # format_ = os.path.splitext(image_name)[-1]
-    # result_image_name = image_name.replace(format_, "_result" + format_)
-    # cv2.imwrite(BACKUP_DIR + result_image_name, image)
+def test_face_croped(image_name, image):
+    
+    image_cropper = CropImage()
+    prediction = np.zeros((1, 3))
+    test_speed = 0
+    # sum the prediction from single model's result
+    for model_name in os.listdir(MODEL_DIR):
+        h_input, w_input, model_type, scale = parse_model_name(model_name)
+        param = {
+            "org_img": image,
+            "bbox": None,
+            "scale": None,
+            "out_w": w_input,
+            "out_h": h_input,
+            "crop": False,
+        }
+        if scale is None:
+            param["crop"] = False
+        img = image_cropper.crop(**param)
+        start = time.time()
+        prediction += model_test.predict(img, os.path.join(MODEL_DIR, model_name))
+        test_speed += time.time()-start
+
+    # draw result of prediction
+    label = np.argmax(prediction)
+    value = prediction[0][label]/2
     return label, value
 
 class Prediction(BaseModel):
@@ -123,13 +125,13 @@ async def predict_color(file: UploadFile = File(...)):
         contents = await file.read()
         image = np.fromstring(contents, np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
+        print(image.shape)
         # image = cv2.imdecode(np.frombuffer(image_file, dtype=np.uint8), -1)
         time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
         number = str(random.randint(0, 10000))
         img_name = time + '_' + number + '.jpg'
 
-        label, value = test(img_name, image)
+        label, value = test_face_croped(img_name, image)
 
         result = {"fake" : str(label), "score": str(value)}
 
